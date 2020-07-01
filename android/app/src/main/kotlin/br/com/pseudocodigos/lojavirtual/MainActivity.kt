5dfc851a9d8d7514e35d5fa943ec4b0bb9a15910
Fernando de Castro <fca.apps@gmail.com>
2020-07-01 18:12:31 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/br/com/pseudocodigos/lojavirtual/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.pseudocodigos.lojavirtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/page_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class PageManager {
+  final PageController _pageController;
+
+  PageManager(this._pageController);
+
+  int page = 0;
+
+  void setPage(int value) {
+    if (value == page)  return;
+    page = value;
+    _pageController.jumpToPage(value);
+  }
+
+}
